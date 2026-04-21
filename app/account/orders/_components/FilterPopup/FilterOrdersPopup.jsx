38359d1c1e1594bf50377fe23a68b90044f83794
Shreya Urkude <shreyaurkude6@gmail.com>
2026-04-21 16:15:06 +0530

    new

--- app/account/orders/_components/FilterPopup/FilterOrdersPopup.jsx
+++ app/account/orders/_components/FilterPopup/FilterOrdersPopup.jsx
@@ -21,7 +21,7 @@ export default function FilterOrdersPopup({
   return (
     <div className={styles.overlay}>
       <div className={styles.popup}>
-        <h2 className={styles.title}>Filters Order</h2>
+        <h2 className={styles.title}>FILTERS ORDERS</h2>
         <div className={styles.body}>
           {/* Status Column */}
           <div className={styles.column}>

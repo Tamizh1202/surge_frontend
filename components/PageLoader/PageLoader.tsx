d12420fc629cfd9f508dd2f2bb4187d06d788834
Tamizh1202 <tamilselvan9359@gmail.com>
2026-06-04 13:31:27 +0530

    changes

--- components/PageLoader/PageLoader.tsx
+++ components/PageLoader/PageLoader.tsx
@@ -1,7 +1,16 @@
+'use client';
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import styles from "./PageLoader.module.css";
 
 export default function PageLoader() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.refresh();
+  }, []);
+
   return (
     <div className={styles.overlay}>
       <Image src="/loader.gif" alt="Loading..." width={150} height={150} unoptimized />
